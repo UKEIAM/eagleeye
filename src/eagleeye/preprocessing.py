5f601fdcc76bbce0f8e11704c772049a2ebe0a36
Michael Blaß <mblass@posteo.net>
2024-03-22 13:21:33 +0100

    refactor: fix type hints

--- src/eagleeye/preprocessing.py
+++ src/eagleeye/preprocessing.py
@@ -1,29 +1,38 @@
 """
 Preprocess idata for chart functions.
 """
+from typing import Any, cast
+
 import arviz as az
 import numpy as np
 import pandas as pd
 
 
-def get_predictions(idata):
-    df = idata.predictions.llh.mean(["chain", "draw"]).to_dataframe().reset_index(drop=True).reset_index()
+FloatArray = np.ndarray[Any, np.dtype[np.float64]]
+
+
+def get_predictions(idata: az.InferenceData) -> pd.DataFrame:
+    df = idata.predictions.llh.mean(["chain", "draw"]).to_dataframe()
+    df = df.reset_index(drop=True).reset_index()
     df.columns = ["index", "prediction"]
-    return df
+    return cast(pd.DataFrame, df)
 
 
-def get_forecast(idata):
+def get_forecast(idata: az.InferenceData) -> pd.DataFrame:
     x = idata.predictions.obs_id_future.to_dataframe()
     y = idata.predictions.yhat_fut.mean(["chain", "draw"]).to_dataframe()
     df = pd.concat((x, y), axis=1)
     df.index.name = None
-    df.columns = ["index", "forecast"]
+    df.columns = pd.Index(["index", "forecast"])
     return df
 
 
-def get_confidence(idata, *, qnt=np.linspace(51, 99, 10), mode="predictions"):
+def get_confidence(idata: az.InferenceData, *,
+                   qnt: FloatArray = np.linspace(51, 99, 10),
+                   mode: str = "predictions"
+                   ) -> list[pd.DataFrame]:
     if mode == "predictions":
-        index = np.arange(idata.predictions.obs_idx.data.size, dtype=int)
+        index = np.arange(idata.predictions.obs_idx.data.size, dtype=np.int32)
         weight = az.extract(idata, group="predictions").llh
     elif mode == "forecast":
         index = idata.predictions.obs_id_future
