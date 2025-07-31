38e194991e5bb59c924ccfeb1622a72460a4a4a5
Michael Blaß <mblass@posteo.net>
2025-07-31 10:38:56 +0200

    tests: apply naming convention for env vars

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-env = os.getenv("EE_TESTDATA_PATH")
+env = os.getenv("EAGLEEYE_TESTDATA_PATH")
 if env is None:
-    raise ValueError("Env var EE_TEST_DATAPATH not set")
+    raise ValueError("Env var EAGLEEYE_TESTDATA_PATH not set")
 TESTDATA_PATH = Path(env)
